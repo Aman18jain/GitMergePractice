6b52b9fae3b6bf8b8d11a98b444df0ca5d9e4964
Aman18jain <amanjain181993@gmail.com>
2020-07-05 11:15:55 -1200

    Adding Project Files

---
+++ index.js
@@ -0,0 +1,17 @@
+document.querySelector('button').addEventListener('click',()=>{
+      
+      let name=document.getElementById('eName').value;
+      let password=document.getElementById('ePassword').value;
+      
+      alert('Name is submitted Successfully');
+
+});
+
+
+
+
+
+
+
+
+
